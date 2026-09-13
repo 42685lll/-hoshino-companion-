8ac7737f5b4e4fea8ce73a2bd5f717aa8222d6ae
42685lll <ljw42685@qq.com>
2026-09-14 07:13:05 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/starhoshino/MainActivity.kt
+++ app/src/main/java/com/example/starhoshino/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.starhoshino
 
-import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val tv = TextView(this)
+        tv.text = "星野已启动 ✅\nWebView 还没接回来"
+        tv.textSize = 22f
+        tv.setPadding(40, 80, 40, 40)
+        setContentView(tv)
     }
 }
