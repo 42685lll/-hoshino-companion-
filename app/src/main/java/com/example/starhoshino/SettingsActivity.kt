1645cc535e81784a58f5b846ad2057429655bc72
42685lll <ljw42685@qq.com>
2026-09-14 06:54:45 +0800

    Update SettingsActivity.kt

--- app/src/main/java/com/example/starhoshino/SettingsActivity.kt
+++ app/src/main/java/com/example/starhoshino/SettingsActivity.kt
@@ -1,20 +1,10 @@
 package com.example.starhoshino
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 
-class SettingsActivity : AppCompatActivity() {
-
+class SettingsActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
-
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.title = "星野设置"
-
-        toolbar.setNavigationOnClickListener { onBackPressed() }
     }
 }
