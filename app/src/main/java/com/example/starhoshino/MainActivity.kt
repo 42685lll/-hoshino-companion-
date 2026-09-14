07e1a77daf6ebe4b83c1ec746ae1abba3c6b80ec
42685lll <ljw42685@qq.com>
2026-09-14 08:25:54 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/starhoshino/MainActivity.kt
+++ app/src/main/java/com/example/starhoshino/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.starhoshino
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import android.webkit.JavascriptInterface
 import android.webkit.WebChromeClient
+import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var webView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,20 +20,51 @@ class MainActivity : AppCompatActivity() {
         webView = WebView(this)
         setContentView(webView)
 
-        webView.settings.apply {
-            javaScriptEnabled = true
-            domStorageEnabled = true
-            allowFileAccess = true
-            allowContentAccess = true
-            mediaPlaybackRequiresUserGesture = false
-            useWideViewPort = true
-            loadWithOverviewMode = true
-        }
+        setupWebView()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled", "JavascriptInterface", "AddJavascriptInterface")
+    private fun setupWebView() {
+        val ws: WebSettings = webView.settings
+        ws.javaScriptEnabled = true
+        ws.domStorageEnabled = true
+        ws.allowFileAccess = true
+        ws.allowContentAccess = true
+        ws.mediaPlaybackRequiresUserGesture = false
+        ws.cacheMode = WebSettings.LOAD_DEFAULT
 
         webView.webViewClient = WebViewClient()
         webView.webChromeClient = WebChromeClient()
 
+        val bridge = NativeBridge()
+        webView.addJavascriptInterface(bridge, "NativeBridge")
+        webView.addJavascriptInterface(bridge, "AndroidBridge")
+
         webView.loadUrl("file:///android_asset/core/index.html")
+    }
+
+    class NativeBridge {
+
+        @JavascriptInterface
+        fun ping(): String {
+            Log.i("STARHOSHINO", "JS ping called")
+            return "pong"
+        }
+
+        @JavascriptInterface
+        fun log(msg: String) {
+            Log.i("STARHOSHINO_JS", msg)
+        }
+
+        @JavascriptInterface
+        fun ready(): String {
+            return "ready"
+        }
+
+        @JavascriptInterface
+        fun getPrompt(): String {
+            return ""
+        }
     }
 
     override fun onBackPressed() {
